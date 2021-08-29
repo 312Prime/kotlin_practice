8212239068a8ee93c0c837c9a2fd0eb993a1b2d2
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-08-29 20:35:45 +0900

    first commit

---
+++ Helloworld/app/src/main/java/kr/co/sI312/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.sI312.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
