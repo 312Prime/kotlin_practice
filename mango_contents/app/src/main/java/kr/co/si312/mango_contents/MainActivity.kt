c065da79d0267d1c7d56bc9feae02dc901c6b474
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-09-22 01:22:03 +0900

    origin

---
+++ mango_contents/app/src/main/java/kr/co/si312/mango_contents/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.si312.mango_contents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
