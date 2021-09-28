5f65a820516af90dcf67a659c719a74ca1385fd1
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-09-28 23:25:58 +0900

    origin

---
+++ mango_contents/app/src/main/java/kr/co/si312/mango_contents/JoinActivity.kt
@@ -0,0 +1,45 @@
+package kr.co.si312.mango_contents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+// ...
+// Initialize Firebase Auth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        val joinBtn = findViewById<Button>(R.id.joinButton)
+        joinBtn.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.emailArea)
+            val password = findViewById<EditText>(R.id.passwordArea)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("JoinActivity", "createUserWithEmail:failure", task.exception)
+
+                    }
+                }
+        }
+    }
+}
--- mango_contents/app/src/main/java/kr/co/si312/mango_contents/SplashActivity.kt
+++ mango_contents/app/src/main/java/kr/co/si312/mango_contents/SplashActivity.kt
@@ -4,18 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        auth = Firebase.auth
+
+        if (auth.currentUser?.uid == null) {
+            //회원가입이 안되어있으므로, JoinActivity
+            Handler().postDelayed({
+                startActivity(Intent(this,JoinActivity::class.java))
+                finish()
+            },3000)
+        } else{
+            //회원가입이 되어있으므로 MainActvity
+
+            Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
             finish()
         },3000)
+        }
+//        Handler().postDelayed({
+//            startActivity(Intent(this,MainActivity::class.java))
+//            finish()
+//        },3000)
 
     }
 }
