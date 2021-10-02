cba8d563e8f283ee7c15ef85fa85e64998126199
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-10-02 22:40:37 +0900

    origin

---
+++ MySoloLife/app/src/main/java/kr/co/si312/mysololife/SplashActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.si312.mysololife
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kr.co.si312.mysololife.auth.IntroActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,IntroActivity::class.java))
+            finish()
+        },3000)
+    }
+}
---
+++ MySoloLife/app/src/main/java/kr/co/si312/mysololife/auth/IntroActivity.kt
@@ -0,0 +1,14 @@
+package kr.co.si312.mysololife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.si312.mysololife.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
