5eff38fe8aa5d7aacb25ce5ea8b18aedb347cdf6
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-10-10 09:30:20 +0900

    origin

--- MySoloLife/app/src/main/java/kr/co/si312/mysololife/Fragments/TalkFragment.kt
+++ MySoloLife/app/src/main/java/kr/co/si312/mysololife/Fragments/TalkFragment.kt
@@ -5,56 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import kr.co.si312.mysololife.R
+import kr.co.si312.mysololife.databinding.FragmentTalkBinding
+import kr.co.si312.mysololife.databinding.FragmentTipBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TalkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TalkFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+        private lateinit var binding: FragmentTalkBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+        }
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            binding = FragmentTalkBinding.inflate(layoutInflater)
+
+            binding = FragmentTalkBinding.inflate(layoutInflater)
+
+            binding.homeTap.setOnClickListener {
+                it.findNavController().navigate(R.id.action_talkFragment_to_homeFragment)
+            }
+            binding.tipTap.setOnClickListener {
+                it.findNavController().navigate(R.id.action_talkFragment_to_tipFragment)
+            }
+            binding.bookmarkTap.setOnClickListener {
+                it.findNavController().navigate(R.id.action_talkFragment_to_bookmarkFragment)
+            }
+            binding.storeTap.setOnClickListener {
+                it.findNavController().navigate(R.id.action_talkFragment_to_storeFragment)
+            }
+
+            return binding.root
         }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_talk, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TalkFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TalkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
