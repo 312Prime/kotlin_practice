3e414ea467f5694fb1df32b00dd42844f76ec8ef
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-10-20 20:12:02 +0900

    origin

--- MySoloLife/app/src/main/java/kr/co/si312/mysololife/board/BoardInsideActivity.kt
+++ MySoloLife/app/src/main/java/kr/co/si312/mysololife/board/BoardInsideActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kr.co.si312.mysololife.R
 import kr.co.si312.mysololife.databinding.ActivityBoardInsideBinding
 import kr.co.si312.mysololife.utils.FBRef
@@ -35,6 +39,27 @@ class BoardInsideActivity : AppCompatActivity() {
         val key = intent.getStringExtra("key")
 
         getBoardData(key.toString())
+        getImageData(key.toString())
+    }
+
+    private fun getImageData(key: String){
+
+        // Reference to an image file in Cloud Storage
+        val storageReference = Firebase.storage.reference.child(key + ".png")
+
+// ImageView in your Activity
+        val imageViewFromFB = binding.getImageArea
+
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+            if (task.isSuccessful){
+
+                Glide.with(this)
+                    .load(task.result)
+                    .into(imageViewFromFB)
+            } else {
+
+            }
+        })
     }
 
     private fun getBoardData(key : String){
--- MySoloLife/app/src/main/java/kr/co/si312/mysololife/board/BoardWriteActivity.kt
+++ MySoloLife/app/src/main/java/kr/co/si312/mysololife/board/BoardWriteActivity.kt
@@ -1,20 +1,30 @@
 package kr.co.si312.mysololife.board
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Gallery
 import android.widget.Toast
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kr.co.si312.mysololife.R
 import kr.co.si312.mysololife.contentsList.BookmarkModel
 import kr.co.si312.mysololife.databinding.ActivityBoardWriteBinding
 import kr.co.si312.mysololife.utils.FBAuth
 import kr.co.si312.mysololife.utils.FBRef
+import java.io.ByteArrayOutputStream
 
 class BoardWriteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBoardWriteBinding
     private val TAG = BoardWriteActivity::class.java.simpleName
+    private var isImageUpload = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityBoardWriteBinding.inflate(layoutInflater)
@@ -32,12 +42,54 @@ class BoardWriteActivity : AppCompatActivity() {
             Log.d(TAG,title)
             Log.d(TAG,content)
 
+            //firebase stroage에 이미지를 저장, 게시글 이미지이름을 키값으로 지정하기위해 키값 미리 가져오기
+            val key = FBRef.boardRef.push().key.toString()
+
             FBRef.boardRef
-                .push()
+                .child(key)
                 .setValue(BoardModel(title,content,uid,time))
 
             Toast.makeText(this,"게시글 작성 완료",Toast.LENGTH_SHORT).show()
+            if (isImageUpload == true) {
+                imageUpload(key)
+            }
+            finish()
+        }
+        binding.imageArea.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+            isImageUpload = true
         }
 
+    }
+
+    private fun imageUpload (key : String){
+
+        val storage = Firebase.storage
+        val storageRef =storage.reference
+        val mountainsRef = storageRef.child(key + ".png")
+
+        val imageView = binding.imageArea
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 100){
+            binding.imageArea.setImageURI(data?.data)
+        }
     }
 }
