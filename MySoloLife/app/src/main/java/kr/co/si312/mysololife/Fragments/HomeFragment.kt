64b5acc3d91054617f54757ac20e7b37d3626d6e
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-10-26 01:12:54 +0900

    origin

--- MySoloLife/app/src/main/java/kr/co/si312/mysololife/Fragments/HomeFragment.kt
+++ MySoloLife/app/src/main/java/kr/co/si312/mysololife/Fragments/HomeFragment.kt
@@ -1,18 +1,33 @@
 package kr.co.si312.mysololife.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import kr.co.si312.mysololife.R
+import kr.co.si312.mysololife.contentsList.BookmarkRVAdapter
+import kr.co.si312.mysololife.contentsList.ContentsModel
 import kr.co.si312.mysololife.databinding.FragmentHomeBinding
+import kr.co.si312.mysololife.utils.FBRef
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+
+    val bookmarkIdList = mutableListOf<String>()
+    val items = ArrayList<ContentsModel>()
+    val itemkeyList = ArrayList<String>()
+
+    lateinit var rvAdapter: BookmarkRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +55,35 @@ class HomeFragment : Fragment() {
         binding.bookmarkTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
         }
+        rvAdapter = BookmarkRVAdapter(requireContext(), items, itemkeyList, bookmarkIdList)
+        val rv: RecyclerView = binding.mainRV
+        rv.adapter = rvAdapter
 
+        rv.layoutManager = GridLayoutManager(requireContext(),2)
+
+        getCategoryData()
 
         return binding.root
+    }
+
+    private fun getCategoryData(){
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (dataModel in dataSnapshot.children) {
+
+                    val item = dataModel.getValue(ContentsModel::class.java)
+                    items.add(item!!)
+                    itemkeyList.add(dataModel.key.toString())
+                }
+                rvAdapter.notifyDataSetChanged()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("ContentsListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.category1.addValueEventListener(postListener)
+        FBRef.category2.addValueEventListener(postListener)
     }
 }
