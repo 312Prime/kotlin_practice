0b9ddc17b497d23caf19ed160db5b54b195a0f9f
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2021-11-04 12:39:32 +0900

    learn

--- Sogating/app/src/main/java/kr/co/si312/sogating/MainActivity.kt
+++ Sogating/app/src/main/java/kr/co/si312/sogating/MainActivity.kt
@@ -2,12 +2,58 @@ package kr.co.si312.sogating
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
+import kr.co.si312.sogating.slider.CardStackAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var cardStackAdapter : CardStackAdapter
+    lateinit var manager : CardStackLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val cardStackView = findViewById<CardStackView>(R.id.cardStackView)
 
+        manager = CardStackLayoutManager(baseContext, object : CardStackListener{
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+
+            }
+
+            override fun onCardRewound() {
+
+            }
+
+            override fun onCardCanceled() {
+
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+
+            }
+
+        })
+
+        val testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
+
+        cardStackAdapter = CardStackAdapter(baseContext,testList)
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = cardStackAdapter
     }
 }
