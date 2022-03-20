ddb18b57bf92b161436829bdfc611771d61000de
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2022-03-20 22:23:43 +0900

    CardStack_xml

---
+++ app/src/main/java/kr/co/si312/sogating/slider/CardStackAdapter.kt
@@ -0,0 +1,33 @@
+package kr.co.si312.sogating.slider
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.si312.sogating.R
+
+class CardStackAdapter(val context: Context, val items : List<String>) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardStackAdapter.ViewHolder {
+
+        val inflater = LayoutInflater.from(parent.context)
+        val view : View = inflater.inflate(R.layout.item_card, parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CardStackAdapter.ViewHolder, position: Int) {
+        holder.binding(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        fun binding(data:String) {
+
+
+        }
+    }
+}
